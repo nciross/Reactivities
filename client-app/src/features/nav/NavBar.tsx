40330bb6049a15f56254837e5637b7de3dd9e1a1
nciross <mohamed.ncir@outlook.com>
2020-01-29 20:36:58 +0100

    End of Section 5

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+interface IProps{
+  openCreateForm:()=>void;
+}
+export const NavBar:React.FC<IProps> = ({openCreateForm}) => {
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+        </Menu.Item>
+        <Menu.Item name="activities" />
+        <Menu.Item>
+          <Button onClick={()=>openCreateForm()} positive content="Create Activiy"  />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -10,3 +10,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ 
